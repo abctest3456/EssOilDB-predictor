ec0cb22130f49625a6c5222a07f0ccbdac4b6382
Ambarish Kumar <36997739+ambarishK@users.noreply.github.com>
2019-04-06 23:04:21 +0530

    Add files via upload
    Python script to process both info sheets.

---
+++ Python3.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+__author__ = "Rob Knight, Gavin Huttley, and Peter Maxwell"
+__copyright__ = "Copyright 2007, The Cogent Project"
+__credits__ = ["Rob Knight", "Peter Maxwell", "Gavin Huttley",
+                    "Matthew Wakefield"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Rob Knight"
+__email__ = "rob@spot.colorado.edu"
+__status__ = "Production"
+
+from collections import defaultdict
+
+
+#paper_code#compund_name#CAS#quant#plant_part#technique#formula#classification#activity#plant_type#condition#IUPAC_name
+f1 = "info_compound_final_upload_01082017.csv"
+
+
+#paper_code#plant_name#geolocation#year#vol#title#plant_family#plant_type
+f2 = "info_plant_final_upload_01082017.csv"
+
+data1 = open(f1).read().splitlines()
+data1 = list(set(data1))
+data2 = open(f2).read().splitlines()
+data2 = list(set(data2))
+
+dic2 = defaultdict(list)
+
+for i in data2:
+    t = i.split("#")[1:]
+    key = t[0]
+    #print(key)
+    dic2[key].append(i)
+
+fh = open("out.csv","w")
+print("paper_code#compund_name#CAS#quant#plant_part#technique#formula#classification#activity#plant_type#condition#IUPAC_name","#paper_code#plant_name#geolocation#year#vol#title#authors#journal#plant_family#plant_type",sep="#",file=fh)
+for i in data1:
+    t = i.split("#")[1:]
+    key = t[0]
+    i = "#".join(t)
+    app = []
+
+    if key in dic2:
+        print(key)
+        app = dic2[key]
+        for n in app:
+            print(i,n,sep="#",file=fh)
+    else:print(i," ",sep="#",file=fh)
+    
+    
+
+
